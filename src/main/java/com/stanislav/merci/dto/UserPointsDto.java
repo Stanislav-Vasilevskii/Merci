e8c94a4cd08e91b478b54765afa8a1d3979d1900
Stanislav <stanislav.java.projects@gmail.com>
2023-05-21 05:48:03 +0300

    Added custom exceptions

--- src/main/java/com/stanislav/merci/dto/UserPointsDto.java
+++ src/main/java/com/stanislav/merci/dto/UserPointsDto.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.stanislav.merci.entity.UserPoints;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
